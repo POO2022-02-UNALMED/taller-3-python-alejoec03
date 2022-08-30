63f38884a747ed55b815c88da449c1b9debf38c0
alejoec03 <juespinosac@unal.edu.co>
2022-08-29 21:41:23 -0500

    :+1:

---
+++ televisores.py
@@ -0,0 +1,119 @@
+from pip._vendor.typing_extensions import Self
+class Marca:
+    def __init__(self, nombre):
+        self._nombre = nombre
+
+    def getNombre(self):
+        return self._nombre
+
+    def setNombre(self, nombre):
+        self._nombre=nombre
+
+class TV:
+    numTV = 0
+    def __init__(self, marca,estado):
+        self._marca = marca
+        self._estado = estado
+        self.canal = 1
+        self.volumen = 1
+        self.precio = 500
+        TV.numTV+=1
+        
+
+    def getMarca(self):
+        return self._marca
+
+    def setMarca(self, marca):
+        self._marca=marca
+
+    def getControl(self):
+        return self._control
+
+    def setControl(self, control):
+        self._control=control
+
+    def getPrecio(self):
+        return self._precio
+
+    def setPrecio(self, precio):
+        self._precio=precio
+    
+    def getVolumen(self):
+        return self.volumen
+    
+    def setVolumen(self, volumen):
+        if(volumen>=0 and volumen<=7 and self.estado == True):
+            self._volumen=volumen
+        
+    def getCanal(self):
+        return self._canal
+
+    def setCanal(self, canal):
+        if(canal>=1 and canal<=120 and self.estado == True ):
+            self._canal=canal
+    
+    def turnOn(self):
+        self._estado = True
+        
+    def turnOff(self):
+        self._estado = False
+    
+    def getEstado(self):
+        return self.estado
+    
+    def canalUp(self):
+        if(self.getEstado() == True and self.canal != 120 ):
+            self.canal+=1
+    def canalDown(self):
+        if(self.getEstado() == True and self.canal != 1 ):
+            self.canal-=1
+    def volumenUp(self):
+        if(self.getEstado() == True and self.volumen != 7 ):
+            self.volumen+=1
+    def volumenDown(self):
+        if(self.getEstado() == True and self.volumen != 0 ):
+            self.volumen-=1
+    @staticmethod
+    def getNumTV():
+        return TV.numTV
+    
+    @staticmethod
+    def setNumTV(num):
+        TV.numTV = num
+        
+class Control:
+    def __init__(self, tv):
+        self.TV = tv
+    
+    def setCanal(self, canal):
+        self.TV.setCanal(canal)
+    
+    def turnOn(self):
+        self.TV.turnOn()
+        
+    def turnOff(self):
+        self.TV.turnOff()
+    
+    def canalUp(self):
+        self.TV.canalUp()
+        
+    def canalDown(self):
+        self.TV.canalDown()
+            
+    def volumenUp(self):
+        self.TV.volumenUp()
+        
+    def volumenDown(self):
+        self.TV.volumenDown()
+    
+    def setTv(self, tv):
+        self.TV = tv
+    
+    def getTv(self):
+        return TV
+    
+    def enlazar(self,tv):
+        self.TV = tv
+        self.TV.setControl(self)
+            
+    
